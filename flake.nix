3883e88a9b2448e7c1cb645b7ad59bf1888503cb
xhuyz <huylovengoc35@gmail.com>
2025-08-08 05:09:46 +0700

    asdf

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.xhuyz = import ./home/xhuyz/develop.nix
+              home-manager.users.xhuyz = import ./home/xhuyz/develop.nix;
               home-manager.backupFileExtension = "backup";
             }
           ];
