7d45db09a7ffc0fc7bc300548f4f2f03e5c946f7
xhuyz <huylovengoc35@gmail.com>
2025-08-08 03:53:46 +0700

    ok

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           modules = [ 
           ./hosts/develop 
           inputs.disko.nixosModules.disko
-          nixos-hardware.nixosModules.fx506hm
+          nixos-hardware.nixosModules.asus-fx506hm
           ];
         };
       };
