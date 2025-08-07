816b7f1500a7996703e22e52ed8b266eba21677d
xhuyz <huylovengoc35@gmail.com>
2025-08-08 05:32:00 +0700

    flake

--- flake.nix
+++ flake.nix
@@ -1,40 +1,54 @@
 {
-  description = ''XhuyZ'';
+  description = ''
+    For questions just DM me on X: https://twitter.com/@m3tam3re
+    There is also some NIXOS content on my YT channel: https://www.youtube.com/@m3tam3re
+
+    One of the best ways to learn NIXOS is to read other peoples configurations. I have personally learned a lot from Gabriel Fontes configs:
+    https://github.com/Misterio77/nix-starter-configs
+    https://github.com/Misterio77/nix-config
+
+    Please also check out the starter configs mentioned above.
+  '';
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     disko.url = "github:nix-community/disko";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = { self, nixpkgs, home-manager, disko, nixos-hardware, ... }@inputs:
+
+  outputs = { self, home-manager, nixpkgs, nixos-hardware, ... }@inputs:
     let
       inherit (self) outputs;
-      systems = [ "x86_64-linux" ];
+      systems = [
+        "x86_64-linux"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
-      packages = forAllSystems (system:
-        import ./pkgs nixpkgs.legacyPackages.${system}
-      );
+      packages =
+        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
       overlays = import ./hosts/overlays { inherit inputs; };
       nixosConfigurations = {
-        develop = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
+        develop = nixpkgs.lib.nixosSystem {    
+            system = "x86_64-linux";
           specialArgs = { inherit inputs outputs; };
-          modules = [
-            ./hosts/develop
-            inputs.disko.nixosModules.disko
-            nixos-hardware.nixosModules.asus-fx506hm
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.backupFileExtension = "backup";
-              home-manager.users.xhuyz = import ./home/xhuyz/develop.nix;
-            }
+          modules = [ 
+          ./hosts/develop 
+          inputs.disko.nixosModules.disko
+          nixos-hardware.nixosModules.asus-fx506hm
           ];
+        };
+      };
+      homeConfigurations = {
+        "xhuyz@develop" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          extraSpecialArgs = { inherit inputs outputs; };
+          hostname = "develop";
+          modules = [ ./home/xhuyz/develop.nix ];
         };
       };
     };
