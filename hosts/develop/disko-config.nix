212eb472e1456483bedc3286522b053225289194
xhuyz <huylovengoc35@gmail.com>
2025-08-07 19:14:53 +0700

    fix efi

--- hosts/develop/disko-config.nix
+++ hosts/develop/disko-config.nix
@@ -18,7 +18,7 @@
               content = {
                 type = "filesystem";
                 format = "vfat";
-                mountpoint = "/boot/EFI";
+                mountpoint = "/boot/efi";
                 mountOptions = ["defaults" "umask=0077"];
               };
             };
