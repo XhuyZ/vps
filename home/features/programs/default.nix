33bbf383ee3aea03ebbba4842f4680ea278f7b3c
xhuyz <huylovengoc35@gmail.com>
2025-08-09 16:10:53 +0700

    remove neovim conflict

--- home/features/programs/default.nix
+++ home/features/programs/default.nix
@@ -5,14 +5,5 @@
   ./starship/default.nix
   ./lazysql/default.nix
   ];
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    withNodeJs = true;
-    withPython3 = true;
-  };
 }
 
