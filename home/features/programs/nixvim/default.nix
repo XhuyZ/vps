21cf8111048574d6767430c8d7495235988e0901
xhuyz <huylovengoc35@gmail.com>
2025-08-09 17:36:24 +0700

    update nixvim homemanage

--- home/features/programs/nixvim/default.nix
+++ home/features/programs/nixvim/default.nix
@@ -10,35 +10,11 @@ in {
     programs.nixvim = {
       enable = true;
       imports = [
+      ./plugins/default.nix
       ./lsp/conform.nix
       ./lsp/fidget.nix
       ./lsp/lsp.nix
       ./lsp/roslyn.nix
-      ./plugins/hlchunk.nix
-      ./plugins/keymaps.nix
-      ./plugins/kulala.nix
-      ./plugins/lazygit.nix
-      ./plugins/lualine.nix
-      ./plugins/markview.nix
-      ./plugins/neoscroll.nix
-      ./plugins/nix-develop.nix
-      ./plugins/noice.nix
-      ./plugins/notify.nix
-      ./plugins/smear-cursor.nix
-      ./plugins/snacks.nix
-      ./plugins/themes.nix
-      ./plugins/timerly.nix
-      ./plugins/tmux-navigator.nix
-      ./plugins/toggleterm.nix
-      ./plugins/treesitter.nix
-      ./plugins/treesj.nix
-      ./plugins/ts-comments.nix
-      ./plugins/web-devicons.nix
-      ./plugins/whichkey.nix
-      ./plugins/wtf.nix
-      ./plugins/yanky.nix
-      ./plugins/yazi.nix
-      ./plugins/zen-mode.nix
       ];
     };
   };
