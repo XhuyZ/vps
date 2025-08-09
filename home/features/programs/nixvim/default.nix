bcc2245871c343d238a62095da60ee85469822c5
xhuyz <huylovengoc35@gmail.com>
2025-08-09 21:56:05 +0700

    update

--- home/features/programs/nixvim/default.nix
+++ home/features/programs/nixvim/default.nix
@@ -7,7 +7,7 @@ in {
     
   config = lib.mkIf cfg.enable {
   home.packages = [ 
-      inputs.nixvim-config.packages.${pkgs.system}.default 
+      inputs.nixvim.packages.${pkgs.system}.default 
     ];
   };
 }
