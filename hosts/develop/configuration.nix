1fd2942db1263da40042ea75e405aeb30c09dacf
xhuyz <huylovengoc35@gmail.com>
2025-08-09 23:48:59 +0700

    update

--- hosts/develop/configuration.nix
+++ hosts/develop/configuration.nix
@@ -40,9 +40,8 @@
   networking.networkmanager.enable = true;
 
   ## --- GUI: GNOME Desktop ---
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-  services.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   services.xserver.xkb = {
     layout = "us";
     variant = "";
