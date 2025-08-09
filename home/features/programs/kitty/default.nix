54269e54886109bf699ba27358cb29c78e8dfc7e
xhuyz <huylovengoc35@gmail.com>
2025-08-09 16:03:58 +0700

    kitty enable

--- home/features/programs/kitty/default.nix
+++ home/features/programs/kitty/default.nix
@@ -1,16 +1,21 @@
-{ config, pkgs, dotfiles, ... }:
+{ config, lib, pkgs, dotfiles, ... }:
+with lib; let
+  cfg = config.features.programs.kitty;
+in {
+  options.features.programs.kitty.enable =
+    mkEnableOption "Enable Kitty terminal emulator";
 
-{
-  programs.kitty = {
-    enable = true;
-    shellIntegration = {
-      enableFishIntegration = true;
-      enableBashIntegration = true;
+  config = mkIf cfg.enable {
+    programs.kitty = {
+      enable = true;
+      shellIntegration = {
+        enableFishIntegration = true;
+        enableBashIntegration = true;
+      };
+      font = { name = "CaskaydiaCove Nerd Font Mono"; };
+      settings = { copy_on_select = "yes"; };
+      extraConfig = builtins.readFile ./kitty.conf;
     };
-    font = {name = "CaskaydiaCove Nerd Font Mono";};
-    settings = {copy_on_select = "yes";};
-    extraConfig = builtins.readFile ./kitty.conf;
   };
 }
-
 
