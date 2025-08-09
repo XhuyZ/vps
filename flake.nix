b36338273084c9c1f7e567714b672a76c3843bc7
xhuyz <huylovengoc35@gmail.com>
2025-08-09 20:17:41 +0700

    update

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
           extraSpecialArgs = { inherit inputs outputs nixvim; };
           hostname = "develop";
           modules = [ 
-          nixvim.homeManagerModules.nixvim 
+          inputs.nixvim.homeManagerModules.nixvim 
           ./home/xhuyz/develop.nix
           ];
         };
