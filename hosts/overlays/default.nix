4558e2b68742a586a93cfa7d8059d0dfdb761599
xhuyz <huylovengoc35@gmail.com>
2025-08-10 23:59:15 +0700

    u

--- hosts/overlays/default.nix
+++ hosts/overlays/default.nix
@@ -9,18 +9,30 @@
     #   ...
     # });
   };
-  nixvim-overlay = final: prev: {
+ nixvim-overlay = final: prev: {
     my-nixvim = inputs.my-nixvim.packages.${prev.system}.default.overrideAttrs (oldAttrs: {
       meta = (oldAttrs.meta or {}) // {
+        available = true;
+        broken = false;
+        changelog = "https://github.com/XhuyZ/nixvim";
         description = "Neovim configured with nixvim";
-        longDescription = "Custom Neovim configuration built with nixvim";
         homepage = "https://github.com/XhuyZ/nixvim";
+        insecure = false;
         license = final.lib.licenses.mit;
+        longDescription = "Custom Neovim configuration built with nixvim";
+        mainProgram = "nvim";
         maintainers = [ ];
+        name = "nixvim-xhuyz";
+        outputsToInstall = [ "out" ];
         platforms = final.lib.platforms.unix;
+        position = null;
+        teams = [ ];
+        teamsPosition = null;
+        unfree = false;
+        unsupported = false;
       };
     });
-  };table-packages = final: _prev: {
+  }; table-packages = final: _prev: {
     stable = import inputs.nixpkgs {
       system = final.system;
       config.allowUnfree = true;
