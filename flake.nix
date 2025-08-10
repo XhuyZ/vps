8935fcf79d882f282f049e4f9d602841bdf09266
xhuyz <huylovengoc35@gmail.com>
2025-08-10 15:25:38 +0700

    update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   '';
   inputs = {
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
