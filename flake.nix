ec08d73ee2b629af8ccd3946f4d082ba5992a724
xhuyz <huylovengoc35@gmail.com>
2025-08-10 23:15:38 +0700

    u

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   description = ''
-  XhuyZ
+    XhuyZ
   '';
+
   inputs = {
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
@@ -13,42 +14,54 @@
     nixvim.url = "github:nix-community/nixvim";
     my-nixvim.url = "github:XhuyZ/nixvim";
   };
+
   outputs = { self, home-manager, nixpkgs, nixos-hardware, nixvim, my-nixvim, ... }@inputs:
     let
       inherit (self) outputs;
-      systems = [
-        "x86_64-linux"
-      ];
+      systems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
-    in {
-      packages =
-        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+
       overlays = import ./hosts/overlays { inherit inputs; };
+
+      mkPkgs = system: import nixpkgs {
+        inherit system;
+        overlays = [
+          overlays.additions
+          overlays.modifications
+          overlays.nixvim-overlay
+          overlays.stable-packages
+        ];
+        config.allowUnfree = true;
+      };
+    in {
+      packages = forAllSystems (system: import ./pkgs (mkPkgs system));
+
+      overlays = overlays;
+
       nixosConfigurations = {
-        develop = nixpkgs.lib.nixosSystem {    
-            system = "x86_64-linux";
+        develop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
           specialArgs = { inherit inputs outputs; };
-          modules = [ 
-          ./hosts/develop 
-          inputs.disko.nixosModules.disko
-          nixos-hardware.nixosModules.asus-fx504gd
-          inputs.home-manager.nixosModules.home-manager
+          pkgs = mkPkgs "x86_64-linux";
+          modules = [
+            ./hosts/develop
+            inputs.disko.nixosModules.disko
+            nixos-hardware.nixosModules.asus-fx504gd
+            inputs.home-manager.nixosModules.home-manager
           ];
         };
       };
+
       homeConfigurations = {
         "xhuyz@develop" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages."x86_64-linux";
+          pkgs = mkPkgs "x86_64-linux";
           extraSpecialArgs = { inherit inputs outputs; };
-          pkgs = import nixpkgs {
-          system = "x86_64-linux";
-          overlays = [ self.overlays ];
-    };
-          modules = [ 
-             nixvim.homeModules.nixvim
+          modules = [
+            nixvim.homeModules.nixvim
             ./home/xhuyz/develop.nix
           ];
         };
       };
     };
 }
+
