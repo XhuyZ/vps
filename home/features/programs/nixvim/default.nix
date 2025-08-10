f8f2a4181e1dd6d4809f555c5fb57a0080023d2f
xhuyz <huylovengoc35@gmail.com>
2025-08-10 15:34:41 +0700

    update

--- home/features/programs/nixvim/default.nix
+++ home/features/programs/nixvim/default.nix
@@ -6,7 +6,7 @@ in {
     lib.mkEnableOption "Enable NixVim configuration";
     
   config = lib.mkIf cfg.enable {
-    programs.my-neovim.enable = true;
+    programs.nixvim.enable = true;
     home.packages = [ pkgs.my-neovim ];
   };
 }
