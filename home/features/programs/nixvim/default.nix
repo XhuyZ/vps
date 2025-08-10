64b4e689b0c15ae4c6fbb17cfba030c7350ac5d5
xhuyz <huylovengoc35@gmail.com>
2025-08-10 15:10:51 +0700

    update

--- home/features/programs/nixvim/default.nix
+++ home/features/programs/nixvim/default.nix
@@ -1,13 +1,16 @@
-{ config, lib, pkgs, inputs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 let
   cfg = config.features.programs.nixvim;
 in {
+  imports = [
+    inputs.my-nixvim.homeManagerModules.nixvim
+  ];
+
   options.features.programs.nixvim.enable =
     lib.mkEnableOption "Enable NixVim configuration";
-    
+
   config = lib.mkIf cfg.enable {
-  home.packages = [ 
-      pkgs.my-neovim
-    ];
+    programs.nixvim.enable = true;
   };
 }
+
