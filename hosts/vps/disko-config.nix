45aec188e12ffe8a29ba837aa5b9adbce5a59ad8
xhuyz <huylovengoc35@gmail.com>
2025-08-13 10:11:50 +0700

    u

--- hosts/vps/disko-config.nix
+++ hosts/vps/disko-config.nix
@@ -1,106 +1,34 @@
-let
-  rawdisk1 = "/dev/sdb";
-in
 {
   disko.devices = {
     disk = {
-      ${rawdisk1} = {
-        device = "${rawdisk1}";
+      main = {
         type = "disk";
+        device = "/dev/sdb"; # CHANGE ME
         content = {
           type = "gpt";
           partitions = {
-            ESP = {
-              label = "EFI";
-              name = "ESP";
-              size = "1024M";
+            boot = {
+              size = "1M";
+              type = "EF02"; # for GRUB MBR
+              priority = 1;
+            };
+            esp = {
+              size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = ["defaults" "umask=0077"];
               };
             };
-            swap = {
-              label = "swap";
-              size = "24G"; # SWAP - Do not Delete this comment
-              content = {
-                type = "swap";
-                randomEncryption = true;
-                resumeDevice = true;
-              };
-            };
-            luks = {
-              label = "encrypted" ;
+            root = {
               size = "100%";
               content = {
-                type = "luks";
-                name = "pool0_0";
-                extraOpenArgs = [ "--allow-discards" ];
-                # if you want to use the key for interactive login be sure there is no trailing newline
-                # for example use `echo -n "password" > /tmp/secret.key`
-                # passwordFile = "/tmp/secret.key"; # Interactive
-                # or file based
-                #settings.keyFile = "/tmp/secret.key";
-                #additionalKeyFiles = ["/tmp/additionalSecret.key"];
-                content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ];
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/root-blank" = {
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/home" = {
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/home/active" = {
-                      mountpoint = "/home";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/home/snapshots" = {
-                      mountpoint = "/home/.snapshots";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/persist" = {
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/persist/active" = {
-                      mountpoint = "/persist";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/persist/snapshots" = {
-                      mountpoint = "/persist/.snapshots";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/var_lib_docker" = {
-                      mountpoint = "/var/lib/docker";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/var_local" = {
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/var_local/active" = {
-                      mountpoint = "/var/local";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/var_local/snapshots" = {
-                      mountpoint = "/var/local/.snapshots";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "/var_log" = {
-                      mountpoint = "/var/log";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                  };
-                };
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+                mountOptions = ["noatime" "nodiratime" "discard"];
               };
             };
           };
@@ -109,3 +37,4 @@ in
     };
   };
 }
+
