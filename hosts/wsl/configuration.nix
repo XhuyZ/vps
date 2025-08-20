90ce9c461eb9b609065469008c7b407eb88380fc
xhuyz <huylovengoc35@gmail.com>
2025-08-20 10:00:38 +0700

    update

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -1,62 +1,26 @@
 { config, inputs, outputs, lib, pkgs, ... }:
-
 {
   imports = [
     <nixos-wsl/modules>
   ];
+  wsl.enable = true;
+  wsl.defaultUser = "xhuyz";
   ## --- Host & Time ---
   networking.hostName = "wsl";
   time.timeZone = "Asia/Ho_Chi_Minh";
   ## --- Locale ---
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "vi_VN";
-    LC_IDENTIFICATION = "vi_VN";
-    LC_MEASUREMENT = "vi_VN";
-    LC_MONETARY = "vi_VN";
-    LC_NAME = "vi_VN";
-    LC_NUMERIC = "vi_VN";
-    LC_PAPER = "vi_VN";
-    LC_TELEPHONE = "vi_VN";
-    LC_TIME = "vi_VN";
-  };
-  ## --- Networking ---
-  networking.networkmanager.enable = true;
-
-  ## --- GUI: GNOME Desktop ---
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
   systemd.targets.sleep.enable = false;
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
-
-
-#   ## --- User ---
-users.users = {
-  xhuyz = {
-    isNormalUser = true;
-    initialPassword = "<><>";
-    extraGroups = [ "wheel" "networkmanager" ]; 
-    packages = [ inputs.home-manager.packages.${pkgs.system}.default ];
-  };
-};
-home-manager = {
+  home-manager = {
   useUserPackages = true;
   extraSpecialArgs = { inherit inputs outputs; };
   users.xhuyz =
     import ../../home/xhuyz/${config.networking.hostName}.nix;
-};
-
-
+  };
   ## --- Unfree packages ---
   nixpkgs.config.allowUnfree = true;
-
   ## --- System packages ---
   environment.systemPackages = with pkgs; [
     inputs.my-nixvim.packages.${system}.default
--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -1,11 +1,19 @@
+{ config, inputs, outputs, lib, pkgs, ... }:
+
 {
   imports = [
-    ./containers
-    # ./traefik.nix
-    ./mysql.nix
-    # ./traefik.nix
-    ./postgres.nix
-    # ./traefik.nix
+    ../common/extraServices
+    ./configuration.nix
+    ./programs.nix
+    ./secrets.nix
+    ./services
   ];
+
+  extraServices = {
+    flatpak.enable = true;
+    ollama.enable = true;
+    podman.enable = true;
+    virtualisation.enable = true;
+  };
 }
 
