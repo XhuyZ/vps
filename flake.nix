95bc4b1388178e287358b192ae5c77786acfa6cf
xhuyz <huylovengoc35@gmail.com>
2025-08-20 09:54:04 +0700

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -67,6 +67,19 @@
           }
           ];
         };
+        wsl = nixpkgs.lib.nixosSystem {    
+            system = "x86_64-linux";
+          specialArgs = { inherit inputs outputs; };
+          modules = [ 
+          ./hosts/wsl
+          inputs.disko.nixosModules.disko
+          nixos-hardware.nixosModules.asus-fx504gd
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.backupFileExtension = "backup";
+          }
+          ];
+        };
       };
       homeConfigurations = {
         # Change networking.hostName option if want to change the hostname of the current system
